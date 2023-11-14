967ea07041756b465e1c6bf01c0e7957ddbbfe74
jungha <jkjungha24@gmail.com>
2023-11-14 19:48:20 +0900

    Fix Login Activity

--- app/src/main/java/com/example/klab2/LoginActivity.kt
+++ app/src/main/java/com/example/klab2/LoginActivity.kt
@@ -11,6 +11,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.klab2.ForgetPasswordActivity
 import com.example.klab2.R
 import com.example.klab2.RegisterActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var usernameEditText: EditText
@@ -39,27 +44,52 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        loginButton.setOnClickListener {
-            // 获取用户输入的用户名和密码
-            val enteredUsername = usernameEditText.text.toString()
-            val enteredPassword = passwordEditText.text.toString()
+        // 从 Shared Preferences 中获取已保存的用户名和密码
+//            val sharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE)
+//            val savedUsername = sharedPreferences.getString("username", "")
+//            val savedPassword = sharedPreferences.getString("password", "")
 
-            // 从 Shared Preferences 中获取已保存的用户名和密码
-            val sharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE)
-            val savedUsername = sharedPreferences.getString("username", "")
-            val savedPassword = sharedPreferences.getString("password", "")
+        val database = Firebase.database
+        val user = database.getReference("users")
+        user.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                loginButton.setOnClickListener {
+                    // 获取用户输入的用户名和密码
+                    val enteredUsername = usernameEditText.text.toString()
+                    val enteredPassword = passwordEditText.text.toString()
+                    // 检查用户名和密码是否匹配
+                    if(!dataSnapshot.hasChild(enteredUsername)){
+                        Toast.makeText(this@LoginActivity, "아이디 없음", Toast.LENGTH_SHORT).show()
+                    }else{
+                        val savedPassword =
+                            dataSnapshot.child(enteredUsername).child("private").child("password").getValue(String::class.java).toString()
+                        if (enteredPassword == savedPassword) {
+                            // 登录成功s
+                            Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                            val sharedPreferences = getSharedPreferences("live", MODE_PRIVATE)
+                            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                            editor.putString("liveuser", enteredUsername)
+                            editor.apply()
 
-            // 检查用户名和密码是否匹配
-            if (enteredUsername == savedUsername && enteredPassword == savedPassword) {
-                // 登录成功
-                Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
-                var Intent = Intent(this, ThingsWantToHearActivity::class.java)
-                startActivity(Intent)
-                // 在这里可以进行登录后的操作，如跳转到主界面
-            } else {
-                // 登录失败
-                Toast.makeText(this@LoginActivity, "아이디나 비밀번호가 잘못되었습니다", Toast.LENGTH_SHORT).show()
+                            var Intent =
+                                Intent(this@LoginActivity, ThingsWantToHearActivity::class.java)
+                            startActivity(Intent)
+                            // 在这里可以进行登录后的操作，如跳转到主界面
+                        } else {
+                            // 登录失败
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "비밀번호가 잘못되었습니다",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
             }
-        }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+        })
+
     }
 }
