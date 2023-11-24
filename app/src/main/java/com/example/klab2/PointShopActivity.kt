d1409ba9ce23a3e7d5778f0b20e0d65525bcd02d
jkjungha <108046448+jkjungha@users.noreply.github.com>
2023-11-24 23:38:25 +0900

    feat point shop activity xml

---
+++ app/src/main/java/com/example/klab2/PointShopActivity.kt
@@ -0,0 +1,11 @@
+package com.example.klab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PointShopActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_point_shop)
+    }
+}
