06e644a977e2a4ff92a25bf08331bd8f7b26c8fb
jkjungha <108046448+jkjungha@users.noreply.github.com>
2023-11-27 19:57:25 +0900

    feat change item activity

--- app/src/main/java/com/example/klab2/ChangeThemeActivity.kt
+++ app/src/main/java/com/example/klab2/ChangeThemeActivity.kt
@@ -1,7 +1,10 @@
 package com.example.klab2
 
+import android.content.ComponentName
+import android.content.Intent
 import android.graphics.ColorMatrix
 import android.graphics.ColorMatrixColorFilter
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,16 +17,23 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-class ChangeThemeActivity : AppCompatActivity() {
+class ChangeThemeActivity : AppCompatActivity(){
+    internal lateinit var player: MediaPlayer
     lateinit var binding: ChangeThemeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ChangeThemeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         init()
     }
 
-    fun init(){
+    fun init() {
+        binding.backButton.setOnClickListener {
+            var intent = Intent(this@ChangeThemeActivity, ThingsWantToHearActivity::class.java)
+            startActivity(intent)
+        }
+
         var user = "userid1"
 //        val sharedPreferences = getSharedPreferences("live", MODE_PRIVATE)
 //        val user = sharedPreferences.getString("user", "")
@@ -32,31 +42,33 @@ class ChangeThemeActivity : AppCompatActivity() {
             Toast.makeText(this@ChangeThemeActivity, "유저를 찾을 수 없음", Toast.LENGTH_SHORT).show()
         }
         val database = Firebase.database
-        var db = database.reference
-        db.addValueEventListener(object : ValueEventListener {
+        var items = database.getReference("users").child(user).child("get_items")
+        items.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                var get_items = dataSnapshot.child("users").child(user).child("get_items")
-                for(child in get_items.children){
-                    var key = child.key
-                    var value = child.child("chose").getValue(Int::class.java)
-                    if(key == "exciting_bgm" && value == 0){
-                        binding.excitingBgmRadioButton.isGone = true
-                    }else if(key == "sea_bgm"){
-                        binding.seaBgmRadioButton.isGone = true
-                    }else if(key == "soft_bgm"){
-                        binding.softBgmRadioButton.isGone = true
-                    }else if(key == "blue_shirt"){
-                        binding.blueShirtRadioButton.isGone = true
-                    }else if(key == "fluff_hat"){
-                        binding.fluffHatRadioButton.isGone = true
-                    }else if(key == "green_pants"){
-                        binding.greenPantsRadioButton.isGone = true
-                    }else if(key == "disco_theme"){
-                        binding.discoThemeRadioButton.isGone = true
-                    }else if(key == "sea_theme"){
-                        binding.seaThemeRadioButton.isGone = true
-                    }else if(key == "forest_theme"){
-                        binding.forestThemeRadioButton.isGone = true
+                for (item in dataSnapshot.children) {
+                    var key = item.key
+                    var bought = item.child("bought").getValue(Int::class.java)
+                    var chose = item.child("chose").getValue(Int::class.java)
+                    if (bought == 0) {
+                        if (key == "exciting_bgm") {
+                            binding.excitingBgmRadioButton.isGone = true
+                        } else if (key == "sea_bgm") {
+                            binding.seaBgmRadioButton.isGone = true
+                        } else if (key == "soft_bgm") {
+                            binding.softBgmRadioButton.isGone = true
+                        } else if (key == "blue_shirt") {
+                            binding.blueShirtRadioButton.isGone = true
+                        } else if (key == "fluff_hat") {
+                            binding.fluffHatRadioButton.isGone = true
+                        } else if (key == "green_pants") {
+                            binding.greenPantsRadioButton.isGone = true
+                        } else if (key == "disco_theme") {
+                            binding.discoThemeRadioButton.isGone = true
+                        } else if (key == "sea_theme") {
+                            binding.seaThemeRadioButton.isGone = true
+                        } else if (key == "forest_theme") {
+                            binding.forestThemeRadioButton.isGone = true
+                        }
                     }
                 }
             }
@@ -66,6 +78,57 @@ class ChangeThemeActivity : AppCompatActivity() {
             }
         })
 
+        binding.bgmRadioGroup.setOnCheckedChangeListener { buttonView, isChecked ->
+            items.child("exciting_bgm/chose").setValue(0)
+            items.child("sea_bgm/chose").setValue(0)
+            items.child("soft_bgm/chose").setValue(0)
+            if(isChecked == binding.excitingBgmRadioButton.id){
+                player = MediaPlayer.create(applicationContext, R.raw.bgm1)
+                player.isLooping = true
+                player.start()
+                items.child("exciting_bgm/chose").setValue(1)
+            }else if(isChecked == binding.seaBgmRadioButton.id){
+                player = MediaPlayer.create(applicationContext, R.raw.bgm2)
+                player.isLooping = true
+                player.start()
+                items.child("sea_bgm/chose").setValue(1)
+            }else if(isChecked == binding.softBgmRadioButton.id){
+                player = MediaPlayer.create(applicationContext, R.raw.bgm3)
+                player.isLooping = true
+                player.start()
+                items.child("soft_bgm/chose").setValue(1)
+            }
+        }
+        binding.clothRadioGroup.setOnCheckedChangeListener { buttonView, isChecked ->
+            items.child("blue_shirt/chose").setValue(0)
+            items.child("fluff_hat/chose").setValue(0)
+            items.child("green_pants/chose").setValue(0)
+            if(isChecked == binding.blueShirtRadioButton.id){
+                items.child("blue_shirt/chose").setValue(1)
+            }else if(isChecked == binding.fluffHatRadioButton.id){
+                items.child("fluff_hat/chose").setValue(1)
+            }else if(isChecked == binding.greenPantsRadioButton.id){
+                items.child("green_pants/chose").setValue(1)
+            }
+        }
+
+        binding.themeRadioGroup.setOnCheckedChangeListener { buttonView, isChecked ->
+            items.child("disco_theme/chose").setValue(0)
+            items.child("sea_theme/chose").setValue(0)
+            items.child("forest_theme/chose").setValue(0)
+            if(isChecked == binding.discoThemeRadioButton.id){
+                items.child("disco_theme/chose").setValue(1)
+            }else if(isChecked == binding.seaThemeRadioButton.id){
+                items.child("sea_theme/chose").setValue(1)
+            }else if(isChecked == binding.forestThemeRadioButton.id){
+                items.child("forest_theme/chose").setValue(1)
+            }
+        }
+
     }
+
+
+
+
 
 }
