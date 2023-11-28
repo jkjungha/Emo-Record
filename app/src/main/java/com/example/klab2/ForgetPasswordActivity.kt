8b022f81b220191ac10134f366011f4067d147c3
ALUSHIJIE <131883112+ALUSHIJIE@users.noreply.github.com>
2023-11-28 13:40:52 +0900

    Update ForgetPasswordActivity.kt

--- app/src/main/java/com/example/klab2/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/klab2/ForgetPasswordActivity.kt
@@ -1,35 +1,24 @@
-package com.example.klab2
-
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
-import com.example.klab2.R
-import com.example.klab2.LoginActivity
-import com.example.klab2.ResetPasswordActivity
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.getValue
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 
-// ForgetPasswordActivity.kt
 class ForgetPasswordActivity : AppCompatActivity() {
+    private lateinit var loadingHelper: LoadingHelper
     private lateinit var resetEmailOrPhoneEditText: EditText
 
     private fun isEmailValid(email: String): Boolean {
-        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+".toRegex()
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
         return email.matches(emailPattern)
     }
 
     private fun isPhoneNumberValid(phoneNumber: String): Boolean {
-        val phonePattern = "^[0-9]{11}$".toRegex()
+        val phonePattern = "^[0-9]{11}$"
         return phoneNumber.matches(phonePattern)
     }
 
@@ -37,60 +26,60 @@ class ForgetPasswordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.forget_password)
 
+        loadingHelper = LoadingHelper(this)
+
         resetEmailOrPhoneEditText = findViewById(R.id.resetEmailOrPhoneEditText)
 
         val resetPasswordButton: Button = findViewById(R.id.resetButton)
         val backToLoginButton: Button = findViewById(R.id.backToLoginButton)
 
-        val database = Firebase.database
-        val user = database.getReference("users")
-        user.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                resetPasswordButton.setOnClickListener {
-                    val emailOrPhone = resetEmailOrPhoneEditText.text.toString()
-                    var bool = false
+        resetPasswordButton.setOnClickListener {
+            // 在点击重置按钮时显示加载动画
+            showLoading()
 
-                    for (child in dataSnapshot.children) {
-                        var validatedEmailOrPhone = child.child("private").child("emailorphone")
-                            .getValue(String::class.java)
-                        if (emailOrPhone == validatedEmailOrPhone) {
-                            // 用户提供的手机号或邮箱与验证成功的匹配
-                            // 执行密码重置逻辑
-                            bool = true
-                            val sharedPreferences = getSharedPreferences("live", MODE_PRIVATE)
-                            val editor: SharedPreferences.Editor = sharedPreferences.edit()
-                            editor.putString("user", child.key)
-                            editor.apply()
-                            Toast.makeText(this@ForgetPasswordActivity, "인증 완료", Toast.LENGTH_SHORT)
-                                .show()
-                            val intent = Intent(
-                                this@ForgetPasswordActivity,
-                                ResetPasswordActivity::class.java
-                            )
-                            startActivity(intent)
-                        }
-                    }
-                    if (!bool) {
-                        Toast.makeText(
-                            this@ForgetPasswordActivity,
-                            "전화번호 또는 이메일 불일치",
-                            Toast.LENGTH_SHORT
-                        )
-                            .show()
-                    }
+            val emailOrPhone = resetEmailOrPhoneEditText.text.toString()
+
+            // 检查是否匹配
+            val sharedPreferences: SharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE)
+            val validatedEmailOrPhone: String = sharedPreferences.getString("validatedEmailOrPhone", "") ?: ""
+
+            // 模拟延时，然后隐藏加载动画
+            Handler().postDelayed({
+                hideLoading()
+
+                if (emailOrPhone == validatedEmailOrPhone) {
+                    // 用户提供的手机号或邮箱与验证成功的匹配
+                    // 执行密码重置逻辑
+                    val intent = Intent(this, ResetPasswordActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // 用户提供的手机号或邮箱与验证成功的不匹配
+                    Toast.makeText(this, "전화번호 또는 이메일 불일치", Toast.LENGTH_SHORT).show()
                 }
-
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-            }
-        })
+            }, 2000) // 调整延时时间
+        }
 
         backToLoginButton.setOnClickListener {
-            // 创建一个 Intent 以返回到登录界面
-            val intent = Intent(this@ForgetPasswordActivity, LoginActivity::class.java)
-            startActivity(intent)
-            finish() // 结束当前的忘记密码界面
+            // 在返回之前显示加载动画
+            showLoading()
+
+            // 模拟延时，然后返回到登录界面
+            Handler().postDelayed({
+                hideLoading()
+
+                // 创建一个 Intent 以返回到登录界面
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish() // 结束当前的忘记密码界面
+            }, 2000) // 调整延时时间
         }
+    }
+
+    private fun showLoading() {
+        loadingHelper.showLoading()
+    }
+
+    private fun hideLoading() {
+        loadingHelper.hideLoading()
     }
 }
