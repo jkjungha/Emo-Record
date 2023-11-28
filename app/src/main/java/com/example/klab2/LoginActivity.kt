f3a36841112162cdde6f1c7be53748fcc19e3a43
ALUSHIJIE <131883112+ALUSHIJIE@users.noreply.github.com>
2023-11-28 13:33:39 +0900

    Update LoginActivity.kt

--- app/src/main/java/com/example/klab2/LoginActivity.kt
+++ app/src/main/java/com/example/klab2/LoginActivity.kt
@@ -1,5 +1,3 @@
-package com.example.klab2
-
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -8,16 +6,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.klab2.ForgetPasswordActivity
-import com.example.klab2.R
-import com.example.klab2.RegisterActivity
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
+import androidx.core.app.ActivityOptionsCompat
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var loadingHelper: LoadingHelper
     private lateinit var usernameEditText: EditText
     private lateinit var passwordEditText: EditText
 
@@ -32,64 +24,56 @@ class LoginActivity : AppCompatActivity() {
         val registerButton: Button = findViewById(R.id.registerButton)
         val forgotPasswordButton: Button = findViewById(R.id.forgotPasswordButton)
 
+        // 初始化 LoadingHelper
+        loadingHelper = LoadingHelper(this)
+
         registerButton.setOnClickListener {
+            // 在注册按钮点击时显示加载动画
+            showLoading()
+
             // 创建一个 Intent 以跳转到注册界面
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
-            startActivity(intent)
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this@LoginActivity)
+            startActivity(intent, options.toBundle())
         }
 
         forgotPasswordButton.setOnClickListener {
+            // 在忘记密码按钮点击时显示加载动画
+            showLoading()
+
             // 创建一个 Intent 以跳转到忘记密码界面
             val intent = Intent(this@LoginActivity, ForgetPasswordActivity::class.java)
-            startActivity(intent)
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this@LoginActivity)
+            startActivity(intent, options.toBundle())
         }
 
-        // 从 Shared Preferences 中获取已保存的用户名和密码
-//            val sharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE)
-//            val savedUsername = sharedPreferences.getString("username", "")
-//            val savedPassword = sharedPreferences.getString("password", "")
+        loginButton.setOnClickListener {
+            // 获取用户输入的用户名和密码
+            val enteredUsername = usernameEditText.text.toString()
+            val enteredPassword = passwordEditText.text.toString()
 
-        val database = Firebase.database
-        val user = database.getReference("users")
-        user.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                loginButton.setOnClickListener {
-                    // 获取用户输入的用户名和密码
-                    val enteredUsername = usernameEditText.text.toString()
-                    val enteredPassword = passwordEditText.text.toString()
-                    // 检查用户名和密码是否匹配
-                    if(!dataSnapshot.hasChild(enteredUsername)){
-                        Toast.makeText(this@LoginActivity, "아이디 없음", Toast.LENGTH_SHORT).show()
-                    }else{
-                        val savedPassword =
-                            dataSnapshot.child(enteredUsername).child("private").child("password").getValue(String::class.java).toString()
-                        if (enteredPassword == savedPassword) {
-                            // 登录成功s
-                            Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
-                            val sharedPreferences = getSharedPreferences("live", MODE_PRIVATE)
-                            val editor: SharedPreferences.Editor = sharedPreferences.edit()
-                            editor.putString("user", enteredUsername)
-                            editor.apply()
+            // 从 Shared Preferences 中获取已保存的用户名和密码
+            val sharedPreferences: SharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE)
+            val savedUsername: String? = sharedPreferences.getString("username", "")
+            val savedPassword: String? = sharedPreferences.getString("password", "")
 
-                            var Intent =
-                                Intent(this@LoginActivity, ThingsWantToHearActivity::class.java)
-                            startActivity(Intent)
-                            // 在这里可以进行登录后的操作，如跳转到主界面
-                        } else {
-                            // 登录失败
-                            Toast.makeText(
-                                this@LoginActivity,
-                                "비밀번호가 잘못되었습니다",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
-                }
+            // 检查用户名和密码是否匹配
+            if (enteredUsername == savedUsername && enteredPassword == savedPassword) {
+                // 登录成功
+                Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                // 在这里可以进行登录后的操作，如跳转到主界面
+            } else {
+                // 登录失败
+                Toast.makeText(this@LoginActivity, "아이디나 비밀번호가 잘못되었습니다", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
 
-            override fun onCancelled(databaseError: DatabaseError) {
-            }
-        })
+    private fun showLoading() {
+        loadingHelper.showLoading()
+    }
 
+    private fun hideLoading() {
+        loadingHelper.hideLoading()
     }
 }
