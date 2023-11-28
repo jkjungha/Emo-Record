b53988a83a8b13a6f4f8cc45d277f98534c81829
ALUSHIJIE <131883112+ALUSHIJIE@users.noreply.github.com>
2023-11-28 13:41:42 +0900

    Update ForgetPasswordActivity.kt

--- app/src/main/java/com/example/klab2/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/klab2/ForgetPasswordActivity.kt
@@ -4,73 +4,48 @@ import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 
-class ForgetPasswordActivity : AppCompatActivity() {
+class ChangeProfileActivity : AppCompatActivity() {
     private lateinit var loadingHelper: LoadingHelper
-    private lateinit var resetEmailOrPhoneEditText: EditText
-
-    private fun isEmailValid(email: String): Boolean {
-        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
-        return email.matches(emailPattern)
-    }
-
-    private fun isPhoneNumberValid(phoneNumber: String): Boolean {
-        val phonePattern = "^[0-9]{11}$"
-        return phoneNumber.matches(phonePattern)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.forget_password)
+        setContentView(R.layout.change_profile)
 
         loadingHelper = LoadingHelper(this)
 
-        resetEmailOrPhoneEditText = findViewById(R.id.resetEmailOrPhoneEditText)
-
-        val resetPasswordButton: Button = findViewById(R.id.resetButton)
+        // 找到UI组件
+        val nameEditText: EditText = findViewById(R.id.editTextName)
+        val saveButton: Button = findViewById(R.id.saveButton)
         val backToLoginButton: Button = findViewById(R.id.backToLoginButton)
 
-        resetPasswordButton.setOnClickListener {
-            // 在点击重置按钮时显示加载动画
+        // 添加保存按钮的点击事件处理逻辑
+        saveButton.setOnClickListener {
+            // 在这里处理保存个人信息的逻辑，并在保存前显示加载动画
             showLoading()
-
-            val emailOrPhone = resetEmailOrPhoneEditText.text.toString()
-
-            // 检查是否匹配
-            val sharedPreferences: SharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE)
-            val validatedEmailOrPhone: String = sharedPreferences.getString("validatedEmailOrPhone", "") ?: ""
 
             // 模拟延时，然后隐藏加载动画
             Handler().postDelayed({
                 hideLoading()
 
-                if (emailOrPhone == validatedEmailOrPhone) {
-                    // 用户提供的手机号或邮箱与验证成功的匹配
-                    // 执行密码重置逻辑
-                    val intent = Intent(this, ResetPasswordActivity::class.java)
-                    startActivity(intent)
-                } else {
-                    // 用户提供的手机号或邮箱与验证成功的不匹配
-                    Toast.makeText(this, "전화번호 또는 이메일 불일치", Toast.LENGTH_SHORT).show()
-                }
+                // 这里添加保存个人信息的实际逻辑
             }, 2000) // 调整延时时间
         }
 
+        // 添加返回按钮的点击事件处理逻辑
         backToLoginButton.setOnClickListener {
             // 在返回之前显示加载动画
             showLoading()
 
-            // 模拟延时，然后返回到登录界面
+            // 模拟延时，然后返回到设置界面
             Handler().postDelayed({
                 hideLoading()
 
-                // 创建一个 Intent 以返回到登录界面
-                val intent = Intent(this, LoginActivity::class.java)
+                // 创建一个 Intent 以返回到设置界面
+                val intent = Intent(this, SettingActivity::class.java)
                 startActivity(intent)
-                finish() // 结束当前的忘记密码界面
+                finish() // 结束当前的修改个人信息界面
             }, 2000) // 调整延时时间
         }
     }
