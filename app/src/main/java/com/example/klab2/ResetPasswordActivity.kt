3e6e53dec6446b9296046a007360287e2113f89b
ALUSHIJIE <131883112+ALUSHIJIE@users.noreply.github.com>
2023-11-28 13:36:18 +0900

    Update ResetPasswordActivity.kt

--- app/src/main/java/com/example/klab2/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/klab2/ResetPasswordActivity.kt
@@ -1,21 +1,14 @@
-package com.example.klab2
-
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.klab2.ForgetPasswordActivity
-import com.example.klab2.R
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 
 class ResetPasswordActivity : AppCompatActivity() {
+    private lateinit var loadingHelper: LoadingHelper
     private lateinit var newPasswordEditText: EditText
     private lateinit var confirmPasswordEditText: EditText
 
@@ -23,52 +16,58 @@ class ResetPasswordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password)
 
+        loadingHelper = LoadingHelper(this)
+
         newPasswordEditText = findViewById(R.id.newPasswordEditText)
         confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText)
 
         val resetButton: Button = findViewById(R.id.resetButton)
         val backToLoginButton: Button = findViewById(R.id.backToLoginButton)
 
-        val database = Firebase.database
-        val user = database.getReference("users")
-        user.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                resetButton.setOnClickListener {
-                    // 获取用户输入的新密码和确认密码
-                    val newPassword = newPasswordEditText.text.toString()
-                    val confirmPassword = confirmPasswordEditText.text.toString()
+        resetButton.setOnClickListener {
+            // 在点击重置按钮时显示加载动画
+            showLoading()
 
-                    // 在这里执行密码重置操作
-                    if (newPassword == confirmPassword) {
-                        // 密码重置成功
-                        val sharedPreferences = getSharedPreferences("live", MODE_PRIVATE)
-                        val savedUsername = sharedPreferences.getString("user", "")
-                        if(savedUsername != null){
-                            user.child(savedUsername).child("private").child("password").setValue(newPassword)
-                            Toast.makeText(this@ResetPasswordActivity, "비밀번호 재설정 완료", Toast.LENGTH_SHORT).show()
-                            var Intent = Intent(this@ResetPasswordActivity, LoginActivity::class.java)
-                            startActivity(Intent)
-                        }else{
-                            Toast.makeText(this@ResetPasswordActivity, "사용자 이름 찾을 수 없음", Toast.LENGTH_SHORT).show()
-                        }
-                        // 在这里可以执行其他操作，如返回登录界面
-                    } else {
-                        // 密码重置失败
-                        Toast.makeText(this@ResetPasswordActivity, "비밀번호가 일치하지 않음", Toast.LENGTH_SHORT)
-                            .show()
-                    }
+            // 获取用户输入的新密码和确认密码
+            val newPassword = newPasswordEditText.text.toString()
+            val confirmPassword = confirmPasswordEditText.text.toString()
+
+            // 模拟延时，然后隐藏加载动画并执行密码重置操作
+            Handler().postDelayed({
+                hideLoading()
+
+                if (newPassword == confirmPassword) {
+                    // 密码重置成功
+                    Toast.makeText(this@ResetPasswordActivity, "비밀번호 재설정 완료", Toast.LENGTH_SHORT).show()
+                    // 在这里可以执行其他操作，如返回登录界面
+                } else {
+                    // 密码重置失败
+                    Toast.makeText(this@ResetPasswordActivity, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
                 }
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-            }
-        })
+            }, 2000) // 调整延时时间
+        }
 
         backToLoginButton.setOnClickListener {
-            // 创建一个 Intent 以返回到登录界面
-            val intent = Intent(this@ResetPasswordActivity, ForgetPasswordActivity::class.java)
-            startActivity(intent)
-            finish() // 结束当前的忘记密码界面
+            // 在返回按钮点击时显示加载动画
+            showLoading()
+
+            // 模拟延时，然后返回到忘记密码界面
+            Handler().postDelayed({
+                hideLoading()
+
+                // 创建一个 Intent 以返回到忘记密码界面
+                val intent = Intent(this@ResetPasswordActivity, ForgetPasswordActivity::class.java)
+                startActivity(intent)
+                finish() // 结束当前的忘记密码界面
+            }, 500) // 调整延时时间
         }
+    }
+
+    private fun showLoading() {
+        loadingHelper.showLoading()
+    }
+
+    private fun hideLoading() {
+        loadingHelper.hideLoading()
     }
 }
