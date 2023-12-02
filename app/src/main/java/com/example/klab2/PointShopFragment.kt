58c144fd7b72a8f0e6d5b307cf71ea0a8a46f9d8
김현지 <freeziahj@gmail.com>
2023-12-02 16:47:43 +0900

    Hyunji

--- app/src/main/java/com/example/klab2/PointShopFragment.kt
+++ app/src/main/java/com/example/klab2/PointShopFragment.kt
@@ -129,10 +129,10 @@ class PointShopFragment : Fragment() {
 
     fun imageCheck() {
 
-        binding.backButton.setOnClickListener {
-            val intent = Intent(context, MainActivity::class.java);
-            startActivity(intent)
-        }
+//        binding.backButton.setOnClickListener {
+//            val intent = Intent(context, MainActivity::class.java);
+//            startActivity(intent)
+//        }
 
         var user = LoginActivity.username
 
