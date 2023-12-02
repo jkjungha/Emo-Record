8fb460dccb60809b3015efb9047058ac668e2682
김현지 <freeziahj@gmail.com>
2023-12-02 17:21:58 +0900

    Hyunji

--- app/src/main/java/com/example/klab2/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/klab2/ChangePasswordActivity.kt
@@ -3,13 +3,18 @@ package com.example.klab2
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.klab2.MainActivity.Companion.select
+import com.google.firebase.database.FirebaseDatabase
 
 class ChangePasswordActivity : AppCompatActivity() {
     private lateinit var loadingHelper: LoadingHelper
+
+    private val db = FirebaseDatabase.getInstance("https://emotion-3bf81-default-rtdb.firebaseio.com/")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,15 +30,25 @@ class ChangePasswordActivity : AppCompatActivity() {
 
         // 添加保存密码按钮的点击事件处理逻辑
         savePasswordButton.setOnClickListener {
+            val password1 = passwordEditText.text.toString()
+            val password2 = confirmPasswordEditText.text.toString()
             // 在这里处理保存密码的逻辑，并在保存前显示加载动画
-            showLoading()
+            if(password1.equals(password2)){
+                showLoading()
+                db.getReference("users").child(LoginActivity.username).child("private").child("password").setValue(confirmPasswordEditText.text.toString())
+                Toast.makeText(this,"비밀번호 수정이 완료되었습니다.",Toast.LENGTH_SHORT)
+                passwordEditText.text.clear()
+                confirmPasswordEditText.text.clear()
+                // 模拟延时，然后隐藏加载动画
+                Handler().postDelayed({
+                    hideLoading()
 
-            // 模拟延时，然后隐藏加载动画
-            Handler().postDelayed({
-                hideLoading()
-
-                // 这里添加保存密码的实际逻辑
-            }, 2000) // 调整延时时间
+                    // 这里添加保存密码的实际逻辑
+                }, 2000) // 调整延时时间
+            }
+            else{
+                Toast.makeText(this,"비밀번호 입력이 잘못되었습니다.",Toast.LENGTH_SHORT)
+            }
         }
 
         // 添加返回按钮的点击事件处理逻辑
--- app/src/main/java/com/example/klab2/ChangeProfileActivity.kt
+++ app/src/main/java/com/example/klab2/ChangeProfileActivity.kt
@@ -7,10 +7,14 @@ import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 class ChangeProfileActivity : AppCompatActivity() {
     private lateinit var loadingHelper: LoadingHelper
+
+    private val db = FirebaseDatabase.getInstance("https://emotion-3bf81-default-rtdb.firebaseio.com/")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,16 +26,26 @@ class ChangeProfileActivity : AppCompatActivity() {
         val nameEditText: EditText = findViewById(R.id.editTextName)
         val saveButton: Button = findViewById(R.id.saveButton)
         val backToLoginButton: Button = findViewById(R.id.backToLoginButton)
+        val name2:EditText = findViewById(R.id.editTextOtherInfo)
 
         // 添加保存按钮的点击事件处理逻辑
         saveButton.setOnClickListener {
-            showLoading()
-            // 在这里处理保存个人信息的逻辑，并在保存前显示加载动画
-            // 模拟延时，然后隐藏加载动画
-            Handler().postDelayed({
-                hideLoading()
-                // 这里添加保存个人信息的实际逻辑
-            }, 2000) // 调整延时时间
+            val name1 = nameEditText.text.toString()
+            val name22 = name2.text.toString()
+            // 在这里处理保存密码的逻辑，并在保存前显示加载动画
+            if (name1.equals(name2)) {
+                showLoading()
+                db.getReference("users").child(LoginActivity.username).setValue(name22)
+                Toast.makeText(this, "비밀번호 수정이 완료되었습니다.", Toast.LENGTH_SHORT)
+                nameEditText.text.clear()
+                name2.text.clear()
+                // 模拟延时，然后隐藏加载动画
+                Handler().postDelayed({
+                    hideLoading()
+
+                    // 这里添加保存密码的实际逻辑
+                }, 2000) // 调整延时时间
+            }
         }
 
         // 添加返回按钮的点击事件处理逻辑
